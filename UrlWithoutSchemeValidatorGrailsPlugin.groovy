e38d6925cc9a422bd784ec6448137628bc859ce8
Marcin Swierczynski <marcin@swierczynski.net>
2014-04-20 15:26:23 +0200

    Plugin metadata

--- UrlWithoutSchemeValidatorGrailsPlugin.groovy
+++ UrlWithoutSchemeValidatorGrailsPlugin.groovy
@@ -2,18 +2,19 @@ class UrlWithoutSchemeValidatorGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.3 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
 
-    // TODO Fill in these fields
     def title = "Url Without Scheme Validator Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = "Marcin Swierczynski"
+    def authorEmail = "marcin@swierczynski.net"
     def description = '''\
-Brief summary/description of the plugin.
+The Url Without Scheme Validator Plugin allows to use custom validator that validates the URLs\
+but - different than standard validator - does not care if the scheme is provided or not.\
+Moreover, it can be used like "first-class", built-in validator.
 '''
 
     // URL to the plugin's documentation
@@ -22,10 +23,10 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "UWS Software Service", url: "http://uws-software-service.com" ]
 
     // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
@@ -34,7 +35,7 @@ Brief summary/description of the plugin.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/MarcinOS/url-without-scheme-validator-plugin" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
