240ac2619f8304ad44a6b9c8bb132eff7b0170c4
Marcin Swierczynski <marcin@swierczynski.net>
2014-04-28 20:11:18 +0200

    Add comments to public methods

--- src/groovy/grails/plugin/urlWithoutSchemeValidator/utils/UrlValidatorUtils.groovy
+++ src/groovy/grails/plugin/urlWithoutSchemeValidator/utils/UrlValidatorUtils.groovy
@@ -11,11 +11,23 @@ import static grails.plugin.urlWithoutSchemeValidator.utils.UrlScheme.*
  */
 class UrlValidatorUtils {
 
+	/**
+	 * Checks if the provided URL is valid, no matter it starts with scheme or not
+	 * @param url The URL to be validated
+	 * @return true if the URL is valid, false otherwise
+	 */
 	public static boolean isValid(final String url) {
 		String urlToCheck = prefixSchemeIfNecessary(url)
 		new UrlValidator().isValid(urlToCheck)
 	}
 
+	/**
+	 * Adds scheme prefix to a given String if necessary, ie. there is no prefix yet.
+	 * By default, adds "http://" but it can be overridden.
+	 * @param url The URL that should be prefixed
+	 * @param scheme The scheme that should be added to the URL. HTTP by default.
+	 * @return The prefixed URL
+	 */
 	public static String prefixSchemeIfNecessary(final String url, final UrlScheme scheme = HTTP) {
 		String urlToCheck = url
 		if (urlToCheck && !startsWithKnownSchemes(urlToCheck)) {
